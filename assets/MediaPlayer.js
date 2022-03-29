2717fd4c39f4b431e69260e0801f257fd325d2a7
Daniel Santos <danielsantosqc.dev@gmail.com>
2022-03-29 02:31:44 -0300

    hasta aqui funciona el play/pause y mute/unmute

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,5 +1,14 @@
 function MediaPlayer(config) {
   this.media = config.element;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function (){
+  this.plugins.forEach(plugin => {
+    plugin.run(this);
+  });
 }
 
 MediaPlayer.prototype.play = function() {
@@ -10,12 +19,28 @@ MediaPlayer.prototype.pause = function() {
   this.media.pause();
 };
 
+MediaPlayer.prototype.mute = function() {
+  this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function() {
+  this.media.muted = false;
+};
+
 MediaPlayer.prototype.togglePlay = function() {
   if (this.media.paused) {
     this.play();
   } else {
     this.pause();
   }
+};
+MediaPlayer.prototype.toggleSound = function(){
+  // if(this.media.muted){
+  //   this.unmute()
+  // }else {
+  //   this.mute()
+  // }
+  this.media.muted ? this.unmute() : this.mute();
 };
 
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,11 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector('.movie');
 const button = document.querySelector('#playButton');
+const muteButton = document.querySelector('#muteButton');
 
-const player = new MediaPlayer({element: video});
+const player = new MediaPlayer({element: video, plugins :[new AutoPlay()]});
 
 button.onclick = () => player.togglePlay();
+muteButton.onclick = () => player.toggleSound();
