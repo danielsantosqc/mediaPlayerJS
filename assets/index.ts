6b50ef52592dc2b344dabf1969a1641416aa993c
Daniel Santos <danielsantos.dev@gmail.com>
2022-05-24 02:40:39 -0300

    update and add code to MediaPlayer and index files

--- assets/index.ts
+++ assets/index.ts
@@ -1,6 +1,7 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause'
+import Ads from './plugins/Ads'
 
 const video = document.querySelector('.movie');
 const button: HTMLButtonElement = document.querySelector('#playButton');
@@ -9,16 +10,37 @@ const muteButton: HTMLButtonElement = document.querySelector('#muteButton');
 const player = new MediaPlayer(
 	{
 		element: video, 
-		plugins :[new AutoPlay(), new AutoPause()],
+		plugins :[new AutoPlay(), new AutoPause(), new Ads()],
 	}
 );
 
 button.onclick = () => player.togglePlay();
 muteButton.onclick = () => player.toggleSound();
 
+document.addEventListener('keyup',(event)=>{
+	switch(event.key){
+		case "p":
+			player.togglePlay();
+			console.log("letra", event.key);
+			break;
+		case "P":
+			player.togglePlay();
+			console.log("letra", event.key);
+			break;
+		case 'm':
+			player.toggleSound();
+			console.log("letra m");
+			break;
+		case 'M':
+			player.toggleSound();
+			console.log("letra m");
+			break;
+	}
+});
+
 // SERVICE WORKER -----------
-if('serviceWorker' in navigator){
-	navigator.serviceWorker.register('/sw.js').catch(error =>{
-		console.log(error.message);
-	})
-}
+// if('serviceWorker' in navigator){
+// 	navigator.serviceWorker.register('/sw.js').catch(error =>{
+// 		console.log(error.message);
+// 	})
+// }
