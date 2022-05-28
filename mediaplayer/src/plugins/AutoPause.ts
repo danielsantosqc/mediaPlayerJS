3f4667601cd0f0e9665549b6843ff5885c45b16b
Daniel Santos <danielsantos.dev@gmail.com>
2022-05-28 20:47:31 -0300

    aun falta configurar los archivos para publicar el plugin a npm

---
+++ mediaplayer/src/plugins/AutoPause.ts
@@ -0,0 +1,52 @@
+import MediaPlayer from "../MediaPlayer";
+class AutoPause{
+	private threshold: number;
+  player: MediaPlayer
+
+	constructor(){
+		this.threshold = 0.3;
+		this.handleIntersection = this.handleIntersection.bind(this)
+		this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
+	}
+	run(player){
+		this.player = player;
+
+		// Intersection Observer---------------
+		// IntersectionObserver(recibe como primer elemento el manipulador, 
+				// como segundo elemento la intersecion de la pantalla, el umbral
+				//  para el uncionamiento del primer elemento)
+		const observer = new IntersectionObserver(this.handleIntersection, {
+			threshold: this.threshold,
+		})
+
+		observer.observe(this.player.media)
+
+		// Visibility Change-----------------
+		document.addEventListener("visibilitychange", this.handleVisibilityChange)
+	}
+
+	private handleIntersection(entries: IntersectionObserverEntry[]){
+		const entry = entries[0];
+		// console.log(entry)
+		const isVisible = entry.intersectionRatio >= this.threshold;
+
+		if (isVisible){
+			this.player.play();
+			// console.log("el video volvio a reproducirse")
+		}else {
+				this.player.pause();
+			// console.log("el video se pauso")
+		}
+	}
+
+	private handleVisibilityChange(){
+		const isVisible = document.visibilityState ==="visible"
+		if(isVisible){
+			this.player.play();
+		}else {
+			this.player.pause();
+		}
+	}
+}
+
+export default AutoPause;
